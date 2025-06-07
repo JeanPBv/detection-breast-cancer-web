6b29eda6829691e51dfa4a259db61d0a2e534d4d
JeanPBv <jeanpierre0452@gmail.com>
2025-06-07 16:53:17 -0500

    feature: Integration Hosting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,20 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import paciente, diagnostico 
 from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 app.mount("/images", StaticFiles(directory="app/images"), name="images")
 
+frontend_url = os.getenv("FRONTEND_URL")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
